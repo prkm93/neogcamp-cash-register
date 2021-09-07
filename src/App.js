21f4fa53990519d66c1d966528f6a8e18a3f28e1
prkm93 <aradshub93@gmail.com>
2021-09-07 09:51:44 +0530

    added styling

--- src/App.js
+++ src/App.js
@@ -4,44 +4,46 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Cash Register</h1>
+      <h1>Cash Register Manager</h1>
       <p>Enter the bill amount and cash given by the customer and know minimum number of notes to return.</p>
       <div className="calculation-section">
         <div className="bill-amount">
           <label htmlFor="billAmount">Bill Amount:</label>
           <br />
-          <input type="number" id="billAmount"/>
+          <input type="number" id="billAmount" className="input-box"/>
         </div>
         <div className="cash-given">
           <label htmlFor="cashGiven">Cash Given:</label>
           <br />
-          <input type="number" id="cashGiven"/>
+          <input type="number" id="cashGiven" className="input-box"/>
         </div>
         <div>
-          <button>Check</button>
+          <button className="btn-check">Check</button>
         </div>
         <h3></h3>
-        <h2>Exchange Amount: </h2>
-        <h1>Return Exchange</h1>
-        <div className="return-exchange-table">
+        <h3>Exchange Amount: </h3>
+        <h2>Return Exchange</h2>
+        <div className="exchange-table">
           <table>
             <tr>
               <th>No of notes</th>
-              <td>123</td>
-              <td>123</td>
-              <td>123</td>
-              <td>123</td>
-              <td>123</td>
-              <td>123</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>
             </tr>
             <tr>
               <th>Note</th>
-              <td>123</td>
-              <td>123</td>
-              <td>123</td>
-              <td>123</td>
-              <td>123</td>
-              <td>123</td>
+              <td>2000</td>
+              <td>500</td>
+              <td>100</td>
+              <td>20</td>
+              <td>10</td>
+              <td>5</td>
+              <td>1</td>
             </tr>
           </table>
         </div>
