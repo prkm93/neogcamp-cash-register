a919df70e24abfa7da7be114d9d9a0665a4bc64a
prkm93 <aradshub93@gmail.com>
2021-09-07 09:02:06 +0530

    Added initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cash Register</h1>
+      <p>Enter the bill amount and cash given by the customer and know minimum number of notes to return.</p>
     </div>
   );
 }
