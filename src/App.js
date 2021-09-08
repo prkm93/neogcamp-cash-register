f5a2f468b4be0a2b67fd87e79e72e56418a64dd9
prkm93 <aradshub93@gmail.com>
2021-09-08 08:35:59 +0530

    added all features

--- src/App.js
+++ src/App.js
@@ -2,6 +2,91 @@ import React, {useState, useRef} from 'react';
 import './App.css';
 
 function App() {
+
+  const [billAmount, setBillAmount] = useState('');
+  const [cashGiven, setCashGiven] = useState('');
+  const [exchangeAmount, setExchangeAmount] = useState('');
+  const [notes, setNotes] = useState({
+    "2000": 0,
+    "500": 0,
+    "100": 0,
+    "20": 0,
+    "10": 0,
+    "5": 0,
+    "1": 0
+  })
+
+  const exchangeHandler = () => {
+    let a = billAmount;
+    let b = cashGiven;
+    let diff; 
+    if ((b-a) > 0) {
+      diff= b - a;
+      setExchangeAmount(diff);
+    } else {
+      setExchangeAmount("Do you wanna wash plates?");
+    }
+
+    let tempExchangeAmount = diff;
+    let tempNotes = notes;
+    console.log(exchangeAmount);
+
+    if ((tempExchangeAmount >= 2000) && Math.floor(tempExchangeAmount/2000)>0) {
+      tempNotes["2000"] = Math.floor(tempExchangeAmount/2000);
+      console.log(tempExchangeAmount);
+      tempExchangeAmount = tempExchangeAmount - Math.floor(tempExchangeAmount/2000)*2000;
+    } 
+
+    if ((tempExchangeAmount >= 500) && Math.floor(tempExchangeAmount)/500){
+      tempNotes["500"] = Math.floor(tempExchangeAmount/500);
+      tempExchangeAmount = tempExchangeAmount - Math.floor(tempExchangeAmount/500)*500;
+    }
+
+    if ((tempExchangeAmount >= 100) && Math.floor(tempExchangeAmount)/100){
+      tempNotes["100"] = Math.floor(tempExchangeAmount/100);
+      tempExchangeAmount = tempExchangeAmount - Math.floor(tempExchangeAmount/100)*100;
+    }
+
+    if ((tempExchangeAmount >= 20) && Math.floor(tempExchangeAmount)/20){
+      tempNotes["20"] = Math.floor(tempExchangeAmount/20);
+      tempExchangeAmount = tempExchangeAmount - Math.floor(tempExchangeAmount/20)*20;
+    }
+
+    if ((tempExchangeAmount >= 10) && Math.floor(tempExchangeAmount)/10){
+      tempNotes["10"] = Math.floor(tempExchangeAmount/10);
+      tempExchangeAmount = tempExchangeAmount - Math.floor(tempExchangeAmount/10)*10;
+    }
+
+    if ((tempExchangeAmount >= 5) && Math.floor(tempExchangeAmount)/5){
+      tempNotes["5"] = Math.floor(tempExchangeAmount/5);
+      tempExchangeAmount = tempExchangeAmount - Math.floor(tempExchangeAmount/5)*5;
+    }
+
+    if ((tempExchangeAmount >= 1) && Math.floor(tempExchangeAmount)/1){
+      tempNotes["1"] = Math.floor(tempExchangeAmount/1);
+      tempExchangeAmount = tempExchangeAmount - Math.floor(tempExchangeAmount/1)*1;
+    }
+    console.log(tempExchangeAmount);
+    console.log(tempNotes);
+
+    setNotes(tempNotes => tempNotes);
+  }
+
+  const resetHandler = () => {
+    setBillAmount("");
+    setCashGiven("");
+    setExchangeAmount("");
+    setNotes({
+      "2000": 0,
+      "500": 0,
+      "100": 0,
+      "20": 0,
+      "10": 0,
+      "5": 0,
+      "1": 0
+    });
+  }
+
   return (
     <div className="App">
       <h1>Cash Register Manager</h1>
@@ -10,40 +95,49 @@ function App() {
         <div className="bill-amount">
           <label htmlFor="billAmount">Bill Amount:</label>
           <br />
-          <input type="number" id="billAmount" className="input-box"/>
+          <input 
+            type="number" 
+            id="billAmount" 
+            className="input-box"
+            value={billAmount}
+            onChange={(e) => setBillAmount(e.target.value)}
+          />
         </div>
         <div className="cash-given">
           <label htmlFor="cashGiven">Cash Given:</label>
           <br />
-          <input type="number" id="cashGiven" className="input-box"/>
+          <input 
+            type="number" 
+            id="cashGiven" 
+            className="input-box"
+            value={cashGiven}
+            onChange={(e) => setCashGiven(e.target.value)}
+          />
         </div>
         <div>
-          <button className="btn-check">Check</button>
+          <button className="btn-check" onClick={exchangeHandler}>Check</button>
+          <button className="btn-check" onClick={resetHandler}>Clear</button>
         </div>
         <h3></h3>
-        <h3>Exchange Amount: </h3>
+        <h3>Exchange Amount: {exchangeAmount}</h3>
         <h2>Return Exchange</h2>
         <div className="exchange-table">
           <table>
             <tr>
               <th>No of notes</th>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
+              {
+                Object.values(notes).reverse().map((note, i) => {
+                  return <td key={i}>{note}</td>
+                })
+              }
             </tr>
             <tr>
               <th>Note</th>
-              <td>2000</td>
-              <td>500</td>
-              <td>100</td>
-              <td>20</td>
-              <td>10</td>
-              <td>5</td>
-              <td>1</td>
+              {
+                Object.keys(notes).sort((a,b) => b-a).map((note, i) => {
+                  return <td key={i}>{note}</td>
+                })
+              }
             </tr>
           </table>
         </div>
